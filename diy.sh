22704ed09fd44d9448c6c94f8d76e71bb2492233
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2022-10-04 10:48:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -45,7 +45,7 @@ popd
 sed -i 's/luci-theme-bootstrap/luci-theme-neobird/' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
