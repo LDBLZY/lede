7680e8067fdcbb79c2ac1984e217c563ad5379e1
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2022-12-02 14:42:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,7 +15,7 @@
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash ./package/lean/luci-app-openclash
 
 # Add luci-app-netdata
-# rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-netdata
 git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata.git ./package/lean/luci-app-netdata
 
 # Add luci-theme-argon
