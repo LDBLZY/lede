62d58dbbb13877ca76ec4611e0c40f9be1ad901a
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2023-01-07 01:10:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall ./package/lean
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./package/lean/openwrt-passwall
 
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 ./package/lean/openwrt-passwall2
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall2 ./package/lean/openwrt-passwall2
+svn co https://github.com/xiaorouji/openwrt-passwall2/branches/luci/luci-app-passwall2 ./package/lean/openwrt-passwall2
 
 # Add luci-app-vssr
 cd ./package/lean/
