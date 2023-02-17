8ccdeecb81a54cb9946f2d579d46f34b72d047b9
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2023-02-17 22:08:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
