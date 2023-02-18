17f472450293fddf9e6e60d65057222177f41454
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2023-02-18 21:22:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,12 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i 'src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-
+#sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 #sed -i '1i src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-
 #sed -i '1i src-git shidahuilang https://github.com/shidahuilang/openwrt-package' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
